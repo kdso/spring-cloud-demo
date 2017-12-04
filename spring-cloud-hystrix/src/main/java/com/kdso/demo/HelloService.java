e963945316eb3a56fbb789fca5b579a263fffb72
kdso <luilei0210@163.com>
2017-12-04 10:06:10 +0800

    hystrix

---
+++ spring-cloud-hystrix/src/main/java/com/kdso/demo/HelloService.java
@@ -0,0 +1,27 @@
+package com.kdso.demo;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by asus on 2017/11/23.
+ */
+@Service
+public class HelloService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    //对该方法创建了熔断器的功能，并指定了fallbackMethod熔断方法
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String hiService(String name) {
+        //均衡负载
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name=" + name, String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
+    }
+}
---
+++ spring-cloud-hystrix/src/main/java/com/kdso/demo/ServiceHystrixApplication.java
@@ -0,0 +1,30 @@
+package com.kdso.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by asus on 2017/11/22.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient //通过@EnableDiscoveryClient向服务中心注册
+@EnableHystrix //@EnableHystrix注解开启Hystrix
+public class ServiceHystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHystrixApplication.class, args);
+    }
+
+    //向程序的ioc注入一个bean: restTemplate;并通过@LoadBalanced注解表明这个restRemplate开启负载均衡的功能
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
